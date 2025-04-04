7a095314237d2740cc99dfef89db0e2d87013b19
YumaIshigooka <114680656+YumaIshigooka@users.noreply.github.com>
2025-04-04 21:39:08 +0200

    Roll a Ball Init

---
+++ Assets/BallBehavior.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0); // Set the initial position of the ball
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
