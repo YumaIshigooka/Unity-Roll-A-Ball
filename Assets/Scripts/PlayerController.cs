bea209efff055b622d4abc023dac43f07c9cd05f
YumaIshigooka <114680656+YumaIshigooka@users.noreply.github.com>
2025-04-05 01:01:20 +0200

    replayable

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,20 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
+using UnityEngine.UIElements;
+using System;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb; 
     private float movementX;
     private float movementY;
-    public float speed = 0; 
     private int count;
+    private int totalCount;
+    private GameObject[] pickups;
+
+
+    public float speed = 0;
+    public TextMeshProUGUI countText; 
+    public GameObject winTextObject;
 
 
 
     void Start() {
         rb = GetComponent <Rigidbody>(); 
         count = 0; 
+        pickups = GameObject.FindGameObjectsWithTag("PickUp");
+        totalCount = pickups.Length;
+        SetCountText();
+        winTextObject.SetActive(false);
     }
 
     void OnMove (InputValue movementValue) {
@@ -24,16 +37,41 @@ public class PlayerController : MonoBehaviour
         movementY = movementVector.y; 
     }
 
-    // Start is called before the first frame update
+   void SetCountText() {
+       countText.text =  "Count: " + count.ToString();
+    }
+
     void FixedUpdate() {
-        Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
-        rb.AddForce(movement * speed);
+        if (count < totalCount) {
+            Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
+            rb.AddForce(movement * speed);
+        }
+    }
+
+    void Update() {
+        if (count >= totalCount && Input.GetKeyDown(KeyCode.R)) {
+            restart();
+        }
     }
 
     void OnTriggerEnter(Collider other) {
         if (other.gameObject.CompareTag("PickUp")) {
             count = count + 1;
+            SetCountText();
             other.gameObject.SetActive(false);
+            if (count >= totalCount) {
+                winTextObject.SetActive(true);
+            }
+        }
+    }
+
+    void restart(){
+        count = 0; 
+        SetCountText();
+        winTextObject.SetActive(false);
+        transform.position = new Vector3(0.0f, 0.5f, 0.0f);
+        foreach (GameObject pickup in pickups) {
+            pickup.SetActive(true);
         }
     }
 
