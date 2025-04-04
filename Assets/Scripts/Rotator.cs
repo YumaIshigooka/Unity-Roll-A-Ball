f24c8f97160971e4471dd8a080b9d91f3b46a4a4
YumaIshigooka <114680656+YumaIshigooka@users.noreply.github.com>
2025-04-04 22:51:21 +0200

    Tutorial Followed until Displaying Score and Text

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -2,18 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallBehavior : MonoBehaviour
+public class Rotator : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(0, 0, 0); // Set the initial position of the ball
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
     }
 }
